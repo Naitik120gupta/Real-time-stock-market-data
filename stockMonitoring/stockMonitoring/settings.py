c4a671b2e13bb3e3a74566e4eecf8eafd07dc85d
Naitik <naitik12gupta05@gmail.com>
2024-11-20 19:31:29 +0530

    Changed urls

--- stockMonitoring/stockMonitoring/settings.py
+++ stockMonitoring/stockMonitoring/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = 'stockMonitoring.asgi.application'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'index/'
 LOGOUT_REDIRECT_URL = '/'
 
 
--- stockMonitoring/stocks/urls.py
+++ stockMonitoring/stocks/urls.py
@@ -3,9 +3,10 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('index/', views.index, name='index'),
     path('api/stock-history/', views.get_stock_history, name='stock_history'),
-     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register, name='register')
 ]
